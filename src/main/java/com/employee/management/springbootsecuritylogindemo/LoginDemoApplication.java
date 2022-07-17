d143c8679fe258044eefe8e3fa8738bf9f9e0248
Ashok Gudise <ashok.gudise@gmail.com>
2022-07-16 23:54:48 -0400

    Updated Project Reference

--- src/main/java/com/employee/management/springbootsecuritylogindemo/LoginDemoApplication.java
+++ src/main/java/com/employee/management/springbootsecuritylogindemo/LoginDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootSecurityLoginDemoApplication {
+public class LoginDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringBootSecurityLoginDemoApplication.class, args);
+        SpringApplication.run(LoginDemoApplication.class, args);
     }
 
 }
